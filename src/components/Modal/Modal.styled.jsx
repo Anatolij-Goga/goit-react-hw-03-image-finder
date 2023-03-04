03f4a4618fea6b4b67e764fe86b35f9e846d9933
Anatolij Goga <anzefal@gmail.com>
2023-03-04 19:53:34 +0200

    Minor fix

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -14,8 +14,8 @@ const Overlay = styled.div`
 `;
 
 const ModalContainer = styled.div`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
+  max-width: calc(100vw - 68px);
+  max-height: calc(100vh - 44px);
 `;
 
 export { Overlay, ModalContainer };
